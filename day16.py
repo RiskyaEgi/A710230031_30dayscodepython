12c9de41776538241c129868601a29010d5ddd3c
RiskyaEgi <riskyaegipratama09@gmail.com>
2024-06-20 09:10:59 +0700

    Create day16.py
    day16

---
+++ day16.py
@@ -0,0 +1,11 @@
+def print_all(items):
+    for items in items:
+        print(items)
+
+my_list = [1,2,3]
+my_tuple= [4,5,6]
+my_string ="Hello, word"
+
+print_all(my_list)
+print_all(my_tuple)
+print_all(my_string)
