714aed19a6f941e9affa36a0d4431377e5b9b22b
RiskyaEgi <riskyaegipratama09@gmail.com>
2024-06-24 05:53:30 +0700

    Create day20.py
    day 20 classs kendaraan

---
+++ day20.py
@@ -0,0 +1,19 @@
+class Mobil:
+    def move(self):
+        print("Berjalan di jalan raya")
+
+class Pesawat:
+    def move(self):
+        print("Terbang di udara")
+
+class Kapal:
+    def move(self):
+        print("Berlayar di lautan")
+
+mobil = Mobil()
+pesawat = Pesawat()
+kapal = Kapal()
+
+mobil.move()
+pesawat.move()
+kapal.move()
